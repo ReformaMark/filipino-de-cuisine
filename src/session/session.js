4e51d69cca8017c22141d0f402c85ca9a14dc85b
ReformaMark <99254035+ReformaMark@users.noreply.github.com>
2023-03-26 09:56:30 +0800

    fix session.js

--- src/session/session.js
+++ src/session/session.js
@@ -1,9 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import { getAuth, EmailAuthProvider, signInWithCredential } from 'firebase/auth';
 
 const SESSION_STORAGE_KEY = 'myapp_session';
-
+const auth = getAuth();
 async function saveSession(user) {
   try {
     const sessionData = JSON.stringify(user.toJSON());
@@ -25,14 +24,14 @@ async function getSession() {
   try {
     const sessionData = await AsyncStorage.getItem(SESSION_STORAGE_KEY);
     if (sessionData) {
-      const user = firebase.auth().currentUser;
+      const user = auth.currentUser;
       if (user) {
         return user;
       } else {
         const userData = JSON.parse(sessionData);
-        const credential = firebase.auth.EmailAuthProvider.credential(userData.email, userData.password);
-        await firebase.auth().signInWithCredential(credential);
-        return firebase.auth().currentUser;
+        const credential = EmailAuthProvider.credential(userData.email, userData.password);
+        await signInWithCredential(auth, credential);
+        return auth.currentUser;
       }
     }
   } catch (error) {
